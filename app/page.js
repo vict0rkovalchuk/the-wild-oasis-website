82a749f24ba6ae692da91d34e6653647fa45550d
Victor Kovalchuk <vict02r05@gmail.com>
2026-05-14 19:00:06 +0300

    creating a layout

--- app/page.js
+++ app/page.js
@@ -1,11 +1,7 @@
-import Navigation from "./components/Navigation";
-
 export default function Page() {
   return (
     <div>
       <h1>The Wild Oasis. Welcome to paradise</h1>
-
-      <Navigation />
     </div>
   );
 }
