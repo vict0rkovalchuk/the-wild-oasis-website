601a13106fefffe091e200c0a0877eac750e86a5
Victor Kovalchuk <vict02r05@gmail.com>
2026-05-15 13:41:54 +0300

    project organization

--- app/_components/Counter.js
+++ app/_components/Counter.js
@@ -2,10 +2,8 @@
 
 import { useState } from "react"
 
-export default function Counter({ users }) {
+export default function Counter() {
   const [count, setCount] = useState(0);
-
-  console.log(users);
 
   return (
     <button onClick={() => setCount(c => c + 1)}>{count}</button>
