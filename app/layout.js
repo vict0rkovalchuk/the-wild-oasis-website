0f2893326346b285305335b1374952c55bd293ce
Victor Kovalchuk <vict02r05@gmail.com>
2026-05-15 14:57:36 +0300

    building the Home Page

--- app/layout.js
+++ app/layout.js
@@ -19,7 +19,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html>
-      <body className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}>
+      <body className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}>
         <Header />
 
         <div className="flex-1 px-8 py-12">
