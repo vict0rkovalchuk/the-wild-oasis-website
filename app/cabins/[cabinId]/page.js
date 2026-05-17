f56f452e90644097369bd90ef19ea180d1eb6050
Victor Kovalchuk <vict02r05@gmail.com>
2026-05-17 17:51:00 +0300

    dynamic route segments: building the cabin page

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -1,27 +1,28 @@
+import { getCabin } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
 
-// PLACEHOLDER DATA
-const cabin = {
-  id: 89,
-  name: "001",
-  maxCapacity: 2,
-  regularPrice: 250,
-  discount: 0,
-  description:
-    "Discover the ultimate luxury getaway for couples in the cozy wooden cabin 001. Nestled in a picturesque forest, this stunning cabin offers a secluded and intimate retreat. Inside, enjoy modern high-quality wood interiors, a comfortable seating area, a fireplace and a fully-equipped kitchen. The plush king-size bed, dressed in fine linens guarantees a peaceful nights sleep. Relax in the spa-like shower and unwind on the private deck with hot tub.",
-  image:
-    "https://dclaevazetcjjkrzczpc.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg",
-};
-
-export default function Page() {
-  const { id, name, maxCapacity, regularPrice, discount, image, description } =
-    cabin;
+export default async function Page({ params: { cabinId } }) {
+  const { 
+    id, 
+    name, 
+    maxCapacity, 
+    regularPrice, 
+    discount, 
+    image, 
+    description 
+  } = await getCabin(cabinId);
 
   return (
     <div className="max-w-6xl mx-auto mt-8">
       <div className="grid grid-cols-[3fr_4fr] gap-20 border border-primary-800 py-3 px-10 mb-24">
         <div className="relative scale-[1.15] -translate-x-3">
-          <img src={image} alt={`Cabin ${name}`} />
+          <Image 
+            src={image} 
+            fill
+            className="object-cover"
+            alt={`Cabin ${name}`} 
+          />
         </div>
 
         <div>
