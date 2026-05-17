5daeb57412da8ead1539f559613a64dfe4570a3e
Victor Kovalchuk <vict02r05@gmail.com>
2026-05-17 17:56:16 +0300

    generating dynamic metadata

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -2,6 +2,14 @@ import { getCabin } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 
+export async function generateMetadata({ params: { cabinId } }) {
+  const { name } = await getCabin(cabinId);
+
+  return {
+    title: `Cabin ${name}`
+  };
+}
+
 export default async function Page({ params: { cabinId } }) {
   const { 
     id, 
